a45ae9365d4c50d641fc2df186ca2e92959d2af6
Jacob Gaumond <jacobgaumond@hotmail.com>
2025-12-01 02:58:53 -0500

    Adds run.sh to automatically execute all test cases using all cpp programs

---
+++ README.md
@@ -0,0 +1,15 @@
+# SYSC4001_A3_P1
+
+## How to use
+
+### Compilation
+
+To compile the cpp files, run `./build.sh`.
+
+Note: Compilation is **necessary** before attempting execution.
+
+### Execution
+
+To execute all three cpp files (e.g., interrupts_EP, interrupts_RR, and interrupts_EP_RR) on all of the input files, run `./run.sh`.
+
+Alternatively, one can manually run the binaries in `/bin` produced during compilation using the input files in `/input_files`
---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Setup output_files dir
+if [ ! -d "output_files" ]; then
+    mkdir output_files
+else
+	rm -r output_files/*
+fi
+
+# Declare variables
+schedulers=("EP"
+            "RR"
+            "EP_RR"
+            )
+
+num_test_cases=10
+
+# Loop over each scheduler type
+for scheduler in "${schedulers[@]}"; do
+    # Setup the output dir for each cpp program
+    mkdir $(echo "output_files/interrupts_${scheduler}_output")
+
+    # Loop over the number of test cases
+    for num in $(seq 1 $num_test_cases); do
+        # Run each test case
+        $(echo "./bin/interrupts_${scheduler} input_files/input${num}.txt")
+
+        # Rename each output file and move it to the output dir of the current scheduler type
+        mv execution.txt $(echo "output_files/interrupts_${scheduler}_output/execution${num}.txt")
+    done
+done
